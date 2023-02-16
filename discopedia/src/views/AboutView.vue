f3c79dc82657b0b7ebd6aac48062063f367a9dbb
Cuba Gustafsson <trill007@hotmail.com>
2023-02-16 11:08:28 +0100

    lite nytt

--- discopedia/src/views/AboutView.vue
+++ discopedia/src/views/AboutView.vue
@@ -3,15 +3,58 @@
 
 <template>
 <main class="container">
-  <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus commodi assumenda mollitia.</h1>
+  <div class="pics">
+  <img src="../assets/pics/chevelle.jpg" alt="chevelle">
+  <img src="../assets/pics/inflames.jpg" alt="chevelle">
+  <img src="../assets/pics/onesidezero.jpg" alt="chevelle">
+  <img src="../assets/pics/tool.jpg" alt="chevelle">
+  <img src="../assets/pics/soen.jpg" alt="chevelle">
+
+  </div>
+  <div class="card">
+    <h1>DISCOPEDIA 2023</h1>
+    <p>Discopedia är en Bandinformationssida utvecklad för information om bands historia, turnédatum, bokning etc</p>
+  </div>
+
 </main>
 </template>
 
 <style scoped>
-.container{
-  background-color: rgb(195, 81, 81);
-  padding:  100px;
-  margin: 100px auto;
-}
+@import url('https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap');
 
+h1{
+  font-family: 'Fredericka the Great', cursive;
+  font-size: 90px;
+}
+p{
+  background-color: rgb(255, 255, 255);
+  padding:  100px;
+  margin: 20px auto;
+  width: 900px;
+  font-size: 30px;
+  font-family: 'Reenie Beanie', cursive;
+  border-radius: 40px;
+}
+.card {
+  padding: 10px;
+  width: 1200px;
+  margin-right: 35px;
+  margin-bottom: 35px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.348);
+  background-color: #caced1;
+  color: black;
+  border-radius: 10px;
+  text-align: center;
+}
+.pics{
+  display: flex;
+  margin: 5px;
+  padding: 5px;
+
+}
+img{
+  margin: 5px;
+  border-radius: 10px;
+}
 </style>
